c9e3bb8c039c2c05ca805ce0c25853fe40c55392
aqua5432 <93812121+aqua5432@users.noreply.github.com>
2023-05-30 16:16:45 +0900

    csvファイルの変更をキーボードで

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ import read_csv, calc
 
 # mainの定義
 def main():
-  df = read_csv.load_csv('row_score.csv')
+  n = input("計算したいcsvファイルを入力してください")
+  df = read_csv.load_csv(n)
   gpa = calc.get_gpa(list(df['評価']), list(df['単位']))
   print("あなたのGPAは，" + str(gpa) + "です．")
 
