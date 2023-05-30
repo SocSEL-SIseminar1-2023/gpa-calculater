f3d212c30852a57c10d0af01c7acb8bf2341c6fe
s147s <134699322+s147s@users.noreply.github.com>
2023-05-30 15:29:09 +0900

    出力されるGPAの値を、有効数字2桁にする

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import read_csv, calc
 def main():
   df = read_csv.load_csv('row_score.csv')
   gpa = calc.get_gpa(list(df['評価']), list(df['単位']))
+  gpa = format(gpa, ".2f")
   print("あなたのGPAは，" + str(gpa) + "です．")
 
 # main関数の実行
