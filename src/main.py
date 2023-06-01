e5e6c9876d3f287aad6ab6f5c42d31b4f38e9beb
aqua5432 <93812121+aqua5432@users.noreply.github.com>
2023-06-01 17:33:52 +0900

    エラー処理の実装

--- src/main.py
+++ src/main.py
@@ -4,7 +4,10 @@ import read_csv, calc
 # mainの定義
 def main():
   n = input("計算したいcsvファイルを入力してください")
-  df = read_csv.load_csv(n)
+  try:
+    df = read_csv.load_csv(n)
+  except FileNotFoundError:
+    print('File not found.')
   gpa = calc.get_gpa(list(df['評価']), list(df['単位']))
   print("あなたのGPAは，" + str(gpa) + "です．")
 
